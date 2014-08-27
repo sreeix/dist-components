129f34543248b40b93ecb116c488163b678899ff
V Sreekanth <sreeix@gmail.com>
2014-08-27 22:04:23 +0530

    Mostly Distributed version.

--- random/ring.erl
+++ random/ring.erl
@@ -1,5 +1,5 @@
 -module(ring).
--export([start_central/3, ring_node_loop/1]).
+-export([start_central/3, ring_node_loop/1, start_distributed/3, ring_node_loop2/2]).
 
 ring_node_loop(NextProcess) ->
     Forward = fun(Message) -> NextProcess ! Message  end,
@@ -26,4 +26,32 @@ send_message(Process, Message, Count) ->
 start_central(M, N, Message) ->
     Root = start_processes(N),
     [send_message(Root, Message, Count) || Count <- lists:seq(1, M)],
+    Root ! {stop}.
+
+
+ring_node_loop2(NextProcess, NumMessages) ->
+    Forward = fun(Message) ->
+		      io:format("Got Message(~p) ~p , Forwarding to ~p ~n", [self(), Message, NextProcess]),
+		      case NextProcess of
+			  nil -> true;
+			  _ -> NextProcess ! Message
+		      end
+	      end,
+    receive
+	{start, 0} -> ring_node_loop2(nil, NumMessages); %% Do nothing
+	{start, N} -> Pid = spawn(ring, ring_node_loop2, [nil, NumMessages]),
+		      Pid ! {start, N -1},
+		      %% io:format("New process: ~p~n", [Pid]),
+		      ring_node_loop2(Pid, NumMessages);
+
+	{message, {Message, Count}} -> Forward({message, {Message, Count}}),
+				       ring_node_loop2(NextProcess, NumMessages);
+	{stop} -> Forward({stop})
+    end.
+
+%% This is obviously a hack. And not fully distributed, but that needs FSM.
+start_distributed(NumMessages, N, Message) ->
+    Root = spawn(ring, ring_node_loop2, [nil, NumMessages]),
+    Root ! {start, N},
+    Root ! {message, {Message, 1}},
     Root ! {stop}.
