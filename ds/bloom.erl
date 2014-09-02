cedb20b44c1bf90a84b3f79c7c417aee26429add
V Sreekanth <sreeix@gmail.com>
2014-09-03 02:36:21 +0530

    Adding a size function and a related hamming distance function.

--- ds/bloom.erl
+++ ds/bloom.erl
@@ -1,5 +1,5 @@
 -module(bloom).
--export([new/2, new/1, add/2, not_exist/2, exist/2]).
+-export([new/2, new/1, add/2, not_exist/2, exist/2, size/1,popcount/1]).
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -18,7 +18,7 @@ add(Item, {HashBinary, NumHashes}) ->
     <<Hash:Size>> = HashBinary,
     NewHash = lists:foldl(fun(X, Acc) -> (erlang:crc32([Item, X]) rem Size) bor Acc end, Hash, lists:seq(1, NumHashes)),
     {<<NewHash:Size>>, NumHashes}.
-
+%% Recreates the same hash and then tries to find
 not_exist(Item, {HashBinary, NumHashes}) ->
     Size = bit_size(HashBinary),
     <<Hash:Size>> = HashBinary,
@@ -29,6 +29,22 @@ not_exist(Item, {HashBinary, NumHashes}) ->
 exist(Item, BT) ->
     not(not_exist(Item, BT)).
 
+
+popcount(0) -> 0;
+popcount(X)
+when is_integer(X), X > 0, X < 16#FFFFFFFF ->
+    ((c4(X) bsr 16) + c4(X)) band 16#0000FFFF.
+c1(V) -> V - ((V bsr 1) band 16#55555555).
+c2(V) -> ((c1(V) bsr 2) band 16#33333333) + (c1(V) band 16#33333333).
+c3(V) -> ((c2(V) bsr 4) + c2(V)) band 16#0F0F0F0F.
+c4(V) -> ((c3(V) bsr 8) + c3(V)) band 16#00FF00FF.
+
+
+%% Gives approximate elements in the filter. Based on blah blah
+size({HashBinary, NumHashes}) ->
+    Size = bit_size(HashBinary),
+    <<Hash:Size>> = HashBinary,
+    (Size * math:log( 1- (popcount(Hash)/Size))) / NumHashes.
 
 bf_test() ->
     BF = bloom:new(10, 2), %% 10 bits empty bloom filter 1- 1023
