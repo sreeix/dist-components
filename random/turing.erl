7b6959279e2bdb742554bf674b087efd5e0e10b8
V Sreekanth <sreeix@gmail.com>
2014-10-18 10:47:12 +0530

    Really working turing machine.

--- random/turing.erl
+++ random/turing.erl
@@ -1,30 +1,58 @@
 -module(turing).
--export([new/1, move/1]).
+-export([new/1, next/1, move/1]).
 
 -include_lib("eunit/include/eunit.hrl").
 
-new( Machine = {{_States, _InputSymbols, _TapeSymbols, _TransitionFun, _StartState, _FinalStates}, _ID = {[_LeftSymbols], _CurrentState, [_RightSymbols]} }) ->
+new( Machine = {{_States, _InputSymbols, _TapeSymbols, _TransitionFun, _StartState, _BlankSymbol, _FinalStates}, _ID = {_LeftSymbols, _CurrentState, _RightSymbols} }) ->
+    %% validated that Input symbols are subset of tape symbols
     Machine.
 
-move({Turing = {_States, _InputSymbols, _TapeSymbols, TransitionFun, _StartState, FinalStates}, ID = {[LeftSymbols], CurrentState, RightSymbols = [CurrentSymbol | RightSymbolsTail]} }) ->
-    Transition = {NewState, SymbolToWrite, _Direction} = TransitionFun(CurrentState, CurrentSymbol),
-    {NewState, SymbolToWrite, Direction} = case  lists:any(fun (X) -> X == NewState end, FinalStates) of
-	true -> io:format("We have an accepting state: ~p ~n", [ID]),
-		{NewState, SymbolToWrite, halt}; %% Halt now.
-	false-> Transition
-    end,
-    NewId = case Direction of
-		halt -> Transition;%% Not sure what to do
-		left -> {lists:droplast(LeftSymbols), NewState, [SymbolToWrite | RightSymbols]};
-		right-> {lists:append(LeftSymbols,[SymbolToWrite]), NewState, RightSymbolsTail}
+next({Turing = {_States, _InputSymbols, _TapeSymbols, _TransitionFun, _StartState, BlankSymbol, _FinalStates}, {LeftSymbols, CurrentState, []} }) ->
+    next({Turing, {LeftSymbols, CurrentState, [BlankSymbol]}});
 
+next({Turing = {_States, _InputSymbols, _TapeSymbols, _TransitionFun, _StartState, BlankSymbol, _FinalStates}, {[], CurrentState, RightSymbols} }) ->
+    next({Turing, {[BlankSymbol],CurrentState, RightSymbols}});
+
+next({Turing = {States, _InputSymbols, TapeSymbols, TransitionFun, _StartState,_BlankSymbol, _FinalStates}, {LeftSymbols, CurrentState, RightSymbols = [CurrentSymbol | RightSymbolsTail]} }) ->
+    {NewState, SymbolToWrite, Direction} = TransitionFun(CurrentState, CurrentSymbol),
+    %% Validate the input symbol is the one in the Tape symbols
+    %% validate the state is in the list of possible states.
+    io:format("New State is ~p writing symbol [~p] to move [~p] ~n", [NewState, SymbolToWrite, Direction]),
+    case contains(NewState, States) of
+	false -> throw("Transition Function output an state unknown in the TM definition");
+	true -> 10
+    end,
+
+    case contains(CurrentSymbol, TapeSymbols) of
+	false -> throw("Current Symbol is not in the Tape Symbol List");
+	true -> 42
+    end,
+
+    NewId = case Direction of
+		halt -> io:format("Halting the turing machine."),
+			{LeftSymbols, NewState, RightSymbols};
+		left -> io:format("Moving left as per the stucture and writing ~p ~p ~n", [SymbolToWrite, NewState]),
+			{lists:droplast(LeftSymbols), NewState, [SymbolToWrite | RightSymbols]};
+		right-> io:format("Moving right as per the stucture and writing ~p ~p ~n", [SymbolToWrite, NewState]),
+			{lists:append(LeftSymbols,[SymbolToWrite]), NewState, RightSymbolsTail}
 	    end,
     {Turing, NewId}.
 
+contains(Item, InList) ->
+    lists:any(fun (X) -> X == Item end, InList).
 
+move(M) ->
+    M2 = { {_, _, _, _, _, _, FinalStates}, Id = {LeftSymbols, NewState, _}} = next(M),
+    format_id(Id),
+    case contains(NewState, FinalStates) of
+	true -> io:format("Final state reached. ~p ~n", [[LeftSymbols]]);
+	false -> move(M2)
+    end.
 
+format_id({Left, State, Right}) ->
+    io:format("~p~p~p~n", [Left, State, Right]).
 
-move_test() ->
+move_to_right_test() ->
     %% Simple machine to set all bits to 0, if 1 it will set the bit to 0 and move right. Till it sees blank, if it sees blank it will enter accepting state and halt.
     TransitionFun = fun (State, TapeSymbol) ->
 			    case TapeSymbol of
@@ -39,5 +67,5 @@ move_test() ->
     %% q2 is the accepting state
     %% b is the blank symbol represented on the tape
 
-    Machine1 = new({ {[q1, q2], [0, 1], [0, 1, blank], TransitionFun, q1, blank, [q2]}, {[], q1, [1, 0, 0]} }),
-    move(Machine1).
+    Machine1 = new({ {[q1, q2], [0, 1], [0, 1, blank], TransitionFun, q1, blank, [q2]}, {[], q1, [1, 0, 1, 0, 1]} }),
+    move(Machine1). %% Should halt.
