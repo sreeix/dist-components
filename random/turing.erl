5bf239152d665ba4967d96df4a71714794490760
V Sreekanth <sreeix@gmail.com>
2015-09-17 01:39:47 +0530

    Tests work.
    Record refactoring had broken the new, fixed and refactored the code

--- random/turing.erl
+++ random/turing.erl
@@ -1,72 +1,81 @@
 -module(turing).
--export([new/1, next/1, move/1]).
+-export([new/1, new/2, next/1, move/1]).
 
 -include_lib("eunit/include/eunit.hrl").
 -record(tape, {leftSymbols=[], currentSymbol=blankSymbol, rightSymbols=[] }).
 
--record(machine, {states=[],inputSymbols=[0,1],tapeSymbols=[0,1, blank], transitionFunction=undefined, startState=undefined, blankSymbol=blank, finalStates=undefined, tape=undefined}).
+-record(machine, {states=[],inputSymbols=[0,1],tapeSymbols=[0,1, blank], transitionFunction=undefined, startState=undefined, blankSymbol=blank, finalStates=undefined}).
 
-new( _Machine = { {States, InputSymbols, TapeSymbols, TransitionFun, StartState, BlankSymbol, FinalStates}, _ID = {LeftSymbols, CurrentSymbol, RightSymbols} }) ->
+new( _Machine =  {States, InputSymbols, TapeSymbols, TransitionFun, StartState, BlankSymbol, FinalStates}, _Tape = {LeftSymbols, CurrentSymbol, RightSymbols} ) ->
     %% validate that Input symbols are subset of tape symbols
     %% Start state is part of all states,
     %% blank symbol is part of tape symbols.
     %% valid final state exists taht is in the States.
     %% transition function is defined.
+    io:format("Creating new of Turning machine ~p ~n", [_Tape]),
     Valid = is_sublist(InputSymbols, TapeSymbols)
         and is_sublist([BlankSymbol], TapeSymbols)
         and is_sublist([StartState], States)
         and (length(FinalStates) > 0) and is_sublist(FinalStates, States)
         and is_function(TransitionFun, 2),
-    io:format("StatesL: InputSymbols: TapeSymbols ~p, ~p, ~p ~p ~p ~n", [is_sublist(InputSymbols, TapeSymbols),is_sublist(TapeSymbols, [StartState]), is_sublist(TapeSymbols, [BlankSymbol]), (length(FinalStates) > 0) and is_sublist(FinalStates, States),is_function(TransitionFun, 2)]),
+    io:format("StatesL: InputSymbols: TapeSymbols ~p, ~p, ~p ~p ~p ~n", [is_sublist(InputSymbols, TapeSymbols),is_sublist(TapeSymbols, [StartState]), is_sublist(TapeSymbols, [BlankSymbol]), (length(FinalStates) > 0) and is_sublist(FinalStates, States), is_function(TransitionFun, 2)]),
         if
-            Valid -> #machine {states= States, inputSymbols= InputSymbols, tapeSymbols= TapeSymbols, transitionFunction= TransitionFun, startState= StartState, blankSymbol=BlankSymbol, finalStates= FinalStates, tape= #tape{leftSymbols=LeftSymbols, currentSymbol= CurrentSymbol, rightSymbols=RightSymbols}};
+            Valid -> {#machine {states= States, inputSymbols= InputSymbols, tapeSymbols= TapeSymbols, transitionFunction= TransitionFun, startState= StartState, blankSymbol=BlankSymbol, finalStates= FinalStates}, StartState, #tape{leftSymbols=LeftSymbols, currentSymbol= CurrentSymbol, rightSymbols=RightSymbols}};
             true -> erlang:error(invalidMachine)
         end.
 
-next({Turing = {_States, _InputSymbols, _TapeSymbols, _TransitionFun, _StartState, BlankSymbol, _FinalStates}, {LeftSymbols, CurrentState, []} }) ->
-    next({Turing, {LeftSymbols, CurrentState, [BlankSymbol]}});
 
-next({Turing = {_States, _InputSymbols, _TapeSymbols, _TransitionFun, _StartState, BlankSymbol, _FinalStates}, {[], CurrentState, RightSymbols} }) ->
-    next({Turing, {[BlankSymbol],CurrentState, RightSymbols}});
+new (Machine) ->
+    new(Machine, {[], blank, []}).
 
-next({Turing = {States, _InputSymbols, TapeSymbols, TransitionFun, _StartState,_BlankSymbol, _FinalStates}, {LeftSymbols, CurrentState, RightSymbols = [CurrentSymbol | RightSymbolsTail]} }) ->
+%% Add blank symbol if the next symbol to the right is empty. In a real turning machine, the tape is supposed to have blanks till infinity, but since we don't want to keep an infinite blank array(we are not hakell), we make it blank lazily.
+next({Machine= #machine{}, CurrentState, Tape=#tape{}})  when Tape#tape.rightSymbols == [] ->
+    next({Machine, CurrentState, Tape#tape{rightSymbols = [Machine#machine.blankSymbol]}});
+
+%% Add blank symbol to the left as well if the left is empty
+next({Machine = #machine{}, CurrentState, Tape= #tape{} })  when Tape#tape.leftSymbols == []->
+    next({Machine, CurrentState, Tape#tape{leftSymbols= [Machine#machine.blankSymbol]}});
+
+next({Machine = #machine{transitionFunction = TransitionFun}, CurrentState, Tape = #tape{leftSymbols = LeftSymbols, currentSymbol = CurrentSymbol, rightSymbols= [RightHead| RightTail]} }) ->
     {NewState, SymbolToWrite, Direction} = TransitionFun(CurrentState, CurrentSymbol),
     %% Validate the input symbol is the one in the Tape symbols
     %% validate the state is in the list of possible states.
     io:format("New State is ~p writing symbol [~p] to move [~p] ~n", [NewState, SymbolToWrite, Direction]),
-    case contains(NewState, States) of
+    case contains(NewState, Machine#machine.states) of
         false -> throw("Transition Function output an state unknown in the TM definition");
-        true -> 10
+        true -> nil %% keep moving.
     end,
 
-    case contains(CurrentSymbol, TapeSymbols) of
+    case contains(CurrentSymbol, Machine#machine.tapeSymbols) of
         false -> throw("Current Symbol is not in the Tape Symbol List");
-        true -> 42 %% doesn't matter i guess.
+        true -> nil %% doesn't matter i guess. Keep moving
     end,
 
-    NewId = case Direction of
+    NewTape = case Direction of
                 halt -> io:format("Halting the turing machine."),
-                        {LeftSymbols, NewState, RightSymbols};
+                        Tape#tape{currentSymbol=SymbolToWrite}; %% Don't mess with the state of the machine, other than updating the new symbol to the curent position on the tape
                 left -> io:format("Moving left as per the stucture and writing ~p ~p ~n", [SymbolToWrite, NewState]),
-                        {lists:droplast(LeftSymbols), NewState, [SymbolToWrite | RightSymbols]};
+                        Tape#tape{leftSymbols = lists:droplast(Tape#tape.leftSymbols), currentSymbol = lists:last(Tape#tape.leftSymbols), rightSymbols = [SymbolToWrite | Tape#tape.rightSymbols]};
+                  %% update current symbol, and move the head left which means that new symbol moves to the right,
                 right-> io:format("Moving right as per the stucture and writing ~p ~p ~n", [SymbolToWrite, NewState]),
-                        {lists:append(LeftSymbols,[SymbolToWrite]), NewState, RightSymbolsTail}
+                        Tape#tape{leftSymbols = LeftSymbols ++ [SymbolToWrite], currentSymbol= RightHead , rightSymbols = RightTail} %% update current Symbole and move to the right
             end,
-    {Turing, NewId}.
+    {Machine, NewState, NewTape}.
 
 contains(Item, InList) ->
     lists:any(fun (X) -> X == Item end, InList).
 
 move(M) ->
-    M2 = { {_, _, _, _, _, _, FinalStates}, Id = {LeftSymbols, NewState, _}} = next(M),
-    format_id(Id),
-    case contains(NewState, FinalStates) of
-        true -> io:format("Final state reached. ~p ~n", [[LeftSymbols]]);
+    M2 = { Machine = #machine{}, NewState, Tape= #tape{}} = next(M),
+    format_id(NewState, Tape),
+    case contains(NewState, Machine#machine.finalStates) of
+        true -> io:format("Final state reached. ~p ~n", [Tape#tape.leftSymbols]),
+                M2;
         false -> move(M2)
     end.
 
-format_id({Left, State, Right}) ->
-    io:format("~p~p~p~n", [Left, State, Right]).
+format_id(State, Tape = #tape{}) ->
+    io:format("~p~p~p~p~n", [Tape#tape.leftSymbols, State,Tape#tape.currentSymbol, Tape#tape.rightSymbols]).
 
 is_sublist(SublistToTest, List)  ->
     lists:all(fun(Elem) -> lists:member(Elem, List) end, SublistToTest ).
@@ -74,10 +83,10 @@ is_sublist(SublistToTest, List)  ->
 
 machine_construction_test() ->
     %% no statesl
-    ?assertError(invalidMachine, new({{[], [], [], undefined, blank, blank, []}, {[],[], []}})),
+    ?assertError(invalidMachine, new( {[], [], [], undefined, blank, blank, []}, {[],[], []} )),
     %% undefined transition
-    ?assertError(invalidMachine, new({{[q1,q2], [], [], undefined, blank, blank, []}, {[],[], []}})),
-    _Machine = new({{[q1,q2], [0,1], [0,1, blank], fun(_Z,_Y) -> 10 end, q1, blank, [q2]}, {[],[], []}}).
+    ?assertError(invalidMachine, new({[q1,q2], [], [], undefined, blank, blank, []}, {[],[], []})),
+    _Machine = new({[q1,q2], [0,1], [0,1, blank], fun(_Z,_Y) -> 10 end, q1, blank, [q2]}, {[],[], []}).
 
 move_to_right_test() ->
     %% Simple machine to set all bits to 0, if 1 it will set the bit to 0 and move right. Till it sees blank, if it sees blank it will enter accepting state and halt.
@@ -94,6 +103,9 @@ move_to_right_test() ->
     %% q2 is the accepting state
     %% b is the blank symbol represented on the tape
 
-    Machine1 = new({ {[q1, q2], [0, 1], [0, 1, blank], TransitionFun, q1, blank, [q2]}, {[], q1, [1, 0, 1, 0, 1]} }),
+    Machine1 = new( {[q1, q2], [0, 1], [0, 1, blank], TransitionFun, q1, blank, [q2]}, {[], blank, [1, 0, 1, 0, 1]} ),
 %%    Machine1 = #machine{states = [q1, q2], transitionFunction=TransitionFun, startState=q1, finalStates=[q2] },
-    move(Machine1). %% Should halt.
+    {_Machine=#machine{}, CurrentState, Tape= #tape{}} = move(Machine1), %% Should halt.
+    CurrentState  = q2,
+    0 = Tape#tape.currentSymbol,
+    io:format("The final State is ~p, and the tape has ~p ~n", [CurrentState, Tape#tape.leftSymbols]).
